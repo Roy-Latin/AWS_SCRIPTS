0f4d3162b00cd45f56a573152305610792007e6f
roy <roeilat@gmail.com>
2023-06-07 15:56:30 +0300

    AMI creation and mechine delete , need work

--- instance_ctl.sh
+++ instance_ctl.sh
@@ -23,7 +23,22 @@ fi
 
 if [ "$1" = "create" ]; then
   echo "creating instance..."
-  aws ec2 run-instances --image-id "ami-0715c1897453cabd1" --instance-type "t2.micro" --key-name "key" --security-group-ids "launch-wizard-1" --subnet-id "subnet-04a1b30c46b166e42" 
-  git clone https://github.com/Roy-Latin/DevOps-Crypto.git /home/ec2-user/
+  new_instance=$(aws ec2 run-instances --image-id "ami-0715c1897453cabd1" --instance-type "t2.micro" --key-name "key" --security-group-ids "sg-0582c9864fa0768cc" --subnet-id "subnet-04a1b30c46b166e42"  --count "1" --output text --query 'Instances[0].InstanceId' --user-data '#!/bin/bash
+sudo yum update -y
+sudo yum install python3 python3-pip python3-devel -y
+pip3 install flask -y
+sudo yum install git -y
+sudo pip install gunicorn -y
+sudo git clone https://github.com/Roy-Latin/DevOps-Crypto.git /home/ec2-user/Git
+sudo echo -e "[Unit]\nDescription=Flask Web Application\nAfter=network.target\n\n[Service]\nUser=ec2-user\nWorkingDirectory=/home/ec2-user/Git\nExecStart=gunicorn --bind 0.0.0.0:5000 app:app &\nRestart=always\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/flaskapp.service
+')
 
+echo "Instance created with ID: $new_instance"
+echo "cloned the git repo into /home/ec2-user/Git"
+sleep 45
+aws ec2 create-image --instance-id $new_instance --name "update" --description "AMI update for flask"
+echo "created AMI"
+sleep 250
+aws ec2 terminate-instances --instance-ids $new_instance
+echo "deleted the mechince with the ID of: $new_instance"
 fi
